f79cb4810d4c52c3a48090161a50b52d0525e1e4
Daniel Mendez <rdmmendez@hotmail.com>
2021-06-07 19:20:06 -0300

    planteo

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,19 +1,40 @@
 package ar.edu.unahur.obj2.tareas
 
-abstract class Tarea(val horasEstimadas: Int, val responsable: Empleado){
-    val listaDeEmpleados = mutableListOf<Empleado>()
-    fun horasNecesarias() = horasEstimadas / listaDeEmpleados.size
-    fun costoDeTarea(){}
+interface Tarea{
+    val responsable: Empleado
+    fun horasNecesarias(): Int
+    fun costoDeLaTarea(): Int
+    fun nominaDeEmpleados(): Int
 }
 
-class Simple(val tiempoTotal: Int, val encargado: Empleado): Tarea(horasEstimadas = tiempoTotal,responsable = encargado){
+class TareaSimple(val costoDeInfraestructura: Int, val horasEstmadas: Int, override val responsable: Empleado): Tarea {
+    val listaDeEmpleado = mutableListOf<Empleado>()
 
+    override fun horasNecesarias(): Int {
+        check(listaDeEmpleado.size == 0){
+            "No hay empleados para repartir tareas"
+        }
+        return horasEstmadas / listaDeEmpleado.size
+    }
+    override fun costoDeLaTarea() =
+        listaDeEmpleado.sumBy { it.sueldo(this.horasNecesarias()) } +
+                this.responsable.sueldo(horasEstmadas) +
+                this.costoDeInfraestructura
+
+    override fun nominaDeEmpleados() = listaDeEmpleado.size + 1 //responsable
 }
-class Integracion(val tiempoTotal: Int, val encargado: Empleado): Tarea(horasEstimadas = tiempoTotal,responsable = encargado){
 
+class TareaDeIntegracion(val encargado: Empleado, override val responsable: Empleado): Tarea{
+    val listaDeSubtareas = mutableListOf<TareaSimple>()
+    fun horas() = listaDeSubtareas.sumBy { it.horasNecesarias() }
+    override fun horasNecesarias() = this.horas() + this.horas()/8
+
+    fun costos() = listaDeSubtareas.sumBy { it.costoDeLaTarea() }
+    override fun costoDeLaTarea() = costos() + costos()* 3 / 100
+
+    override fun nominaDeEmpleados() = listaDeSubtareas.sumBy { it.nominaDeEmpleados() } + 1 // Encargado
 }
-
-
 
 class Empleado(val costoPorHora: Int) {
+    fun sueldo(horasDeTrabajo: Int) = costoPorHora * horasDeTrabajo
 }
