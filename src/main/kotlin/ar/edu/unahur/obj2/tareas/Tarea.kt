47f7b29b5fbfb8e6aa10a20d9522fdea7c803624
Daniel Mendez <rdmmendez@hotmail.com>
2021-06-07 14:26:09 -0300

    Primeros pasoso 2

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,12 +1,19 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea( val horasEstimadas: Int, val responsable: Empleado){
+abstract class Tarea(val horasEstimadas: Int, val responsable: Empleado){
     val listaDeEmpleados = mutableListOf<Empleado>()
-
-    fun horasNecesarias(){}
+    fun horasNecesarias() = horasEstimadas / listaDeEmpleados.size
     fun costoDeTarea(){}
 }
 
-class Empleado {
+class Simple(val tiempoTotal: Int, val encargado: Empleado): Tarea(horasEstimadas = tiempoTotal,responsable = encargado){
 
+}
+class Integracion(val tiempoTotal: Int, val encargado: Empleado): Tarea(horasEstimadas = tiempoTotal,responsable = encargado){
+
+}
+
+
+
+class Empleado(val costoPorHora: Int) {
 }
