3439a4b5e2c4d4e3bd20b6f993213ca35e503f4b
FrancoRueta <askjfuhje@hotmail.com>
2021-06-08 17:45:40 -0300

    Ejercicio completamente testeados, arregladas inconsistencias y detalles del codigo.

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -7,30 +7,33 @@ interface Tarea{
     fun nominaDeEmpleados(): Int
 }
 
-class TareaSimple(val costoDeInfraestructura: Int, val horasEstmadas: Int, override val responsable: Empleado): Tarea {
+class TareaSimple(val costoDeInfraestructura: Int, val horasEstimadas: Int, override val responsable: Empleado): Tarea {
     val listaDeEmpleado = mutableListOf<Empleado>()
 
     override fun horasNecesarias(): Int {
-        check(listaDeEmpleado.size == 0){
+        check(listaDeEmpleado.size != 0){ //el check funciona al revez, si esta en == no anda.
             "No hay empleados para repartir tareas"
         }
-        return horasEstmadas / listaDeEmpleado.size
+        return horasEstimadas / listaDeEmpleado.size
     }
     override fun costoDeLaTarea() =
         listaDeEmpleado.sumBy { it.sueldo(this.horasNecesarias()) } +
-                this.responsable.sueldo(horasEstmadas) +
+                this.responsable.sueldo(horasEstimadas) +
                 this.costoDeInfraestructura
 
-    override fun nominaDeEmpleados() = listaDeEmpleado.size + 1 //responsable
+    override fun nominaDeEmpleados() = listaDeEmpleado.size + 1
 }
 
-class TareaDeIntegracion(val encargado: Empleado, override val responsable: Empleado): Tarea{
+class TareaDeIntegracion(override val responsable: Empleado): Tarea{ //encargado innecesario
     val listaDeSubtareas = mutableListOf<Tarea>()
-    fun horas() = listaDeSubtareas.sumBy { it.horasNecesarias() }
-    override fun horasNecesarias() = this.horas() + this.horas()/8
+
+    fun horasDeSubtareas() = listaDeSubtareas.sumBy { it.horasNecesarias() }
+    override fun horasNecesarias() = this.horasDeSubtareas() + (this.horasDeSubtareas() / 8)
+
 
     fun costos() = listaDeSubtareas.sumBy { it.costoDeLaTarea() }
-    override fun costoDeLaTarea() = costos() + costos()* 3 / 100
+    override fun costoDeLaTarea() = costos() + (costos()* 0.03).toInt() //mas sencillo
+
 
     override fun nominaDeEmpleados() = listaDeSubtareas.sumBy { it.nominaDeEmpleados() } + 1 // Encargado
 }
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,94 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 
 class TareaTest : DescribeSpec({
-  describe("Una tarea") {
+  describe("Una tarea simple"){
+    it("se puede crear"){
+      val eduardoResponsable = Empleado(200)
+      val tareaMantenimiento = TareaSimple(13000,16,eduardoResponsable)
+      tareaMantenimiento.shouldBeInstanceOf<TareaSimple>()
+    }
+    it("tiene horas necesarias para terminar el projecto."){
+      //horas estimadas dividido cantidad de empleados (sin responsable.)
+      val eduardoResponsable = Empleado(200)
+      val pepeLaburante = Empleado(100)
+      val mariaLaburante = Empleado(100)
+      val tareaMantenimiento = TareaSimple(13000,16,eduardoResponsable)
+      tareaMantenimiento.listaDeEmpleado.add(pepeLaburante)
+      tareaMantenimiento.listaDeEmpleado.add(mariaLaburante)
+      tareaMantenimiento.horasNecesarias().shouldBe(8)
+    }
+    it("tiene un costo."){
+      val eduardoResponsable = Empleado(300) //4800
+      val pepeLaburante = Empleado(100) //400
+      val sofiaLaburante = Empleado(150) //600
+      val juanLaburante = Empleado(100) //400
+      val lucilaLaburante = Empleado(150) //600
+      val tareaMantenimiento = TareaSimple(13000,16,eduardoResponsable) //13k
+      tareaMantenimiento.listaDeEmpleado.add(pepeLaburante)
+      tareaMantenimiento.listaDeEmpleado.add(sofiaLaburante)
+      tareaMantenimiento.listaDeEmpleado.add(juanLaburante)
+      tareaMantenimiento.listaDeEmpleado.add(lucilaLaburante)
+      tareaMantenimiento.horasNecesarias().shouldBe(4)
+      tareaMantenimiento.costoDeLaTarea().shouldBe(19800)
+    }
+  }
+  describe("Una tarea de integracion"){
+    //2 tareas para usar dentro de los tests:
+    val eduardoResponsable = Empleado(300)
+    val pepeLaburante = Empleado(100)
+    val sofiaLaburante = Empleado(150)
+    val juanLaburante = Empleado(100)
+    val lucilaLaburante = Empleado(150)
+    val tareaMantenimiento = TareaSimple(13000,16,eduardoResponsable)
+    tareaMantenimiento.listaDeEmpleado.add(pepeLaburante)
+    tareaMantenimiento.listaDeEmpleado.add(sofiaLaburante)
+    tareaMantenimiento.listaDeEmpleado.add(juanLaburante)
+    tareaMantenimiento.listaDeEmpleado.add(lucilaLaburante)
+    //---
+    val pepitoResponsable = Empleado(150)
+    val pipoLaburante = Empleado(50)
+    val candyLaburante = Empleado(75)
+    val agusLaburante = Empleado(50)
+    val antoLaburante = Empleado(75)
+    val tareaDesayuno = TareaSimple(6500,32,pepitoResponsable)
+    tareaDesayuno.listaDeEmpleado.add(pipoLaburante)
+    tareaDesayuno.listaDeEmpleado.add(candyLaburante)
+    tareaDesayuno.listaDeEmpleado.add(agusLaburante)
+    tareaDesayuno.listaDeEmpleado.add(antoLaburante)
+    //-------------------------------------------------------------------------
+    it("Se puede crear"){
+      val tareaDeIntegracion = TareaDeIntegracion(eduardoResponsable)
+      tareaDeIntegracion.listaDeSubtareas.add(tareaMantenimiento)
+      tareaDeIntegracion.shouldBeInstanceOf<TareaDeIntegracion>()
+    }
+    it("Tiene horas necesarias para realizarse"){
+      val jesusResponsable = Empleado(1000)
+      val tareaDeMantenerDesayunos = TareaDeIntegracion(jesusResponsable)
+      tareaDeMantenerDesayunos.listaDeSubtareas.add(tareaMantenimiento)
+      tareaDeMantenerDesayunos.listaDeSubtareas.add(tareaDesayuno)
+      //(8 de desayuno + 4 de mantenimiento) + 1 hora por cada 8 horas de trabajo
+      tareaDeMantenerDesayunos.horasNecesarias().shouldBe(13)
+    }
+    it("tiene un costo"){
+      val jesusResponsable = Empleado(1000)
+      val tareaDeMantenerDesayunos = TareaDeIntegracion(jesusResponsable)
+      tareaDeMantenerDesayunos.listaDeSubtareas.add(tareaMantenimiento) //costo = 19800
+      tareaDeMantenerDesayunos.listaDeSubtareas.add(tareaDesayuno) //costo = 13300
+      tareaMantenimiento.costoDeLaTarea().shouldBe(19800)
+      tareaDesayuno.costoDeLaTarea().shouldBe(13300)
+      //costoTarea = 19800 + 13300 = (33100) + 33100 * 0.03(993)
+      tareaDeMantenerDesayunos.costoDeLaTarea().shouldBe(34093)
+    }
+    it("tiene una nomina de empleados"){
+      val jesusResponsable = Empleado(1000)
+      val tareaDeMantenerDesayunos = TareaDeIntegracion(jesusResponsable)
+      tareaDeMantenerDesayunos.listaDeSubtareas.add(tareaMantenimiento) //costo = 19800
+      tareaDeMantenerDesayunos.listaDeSubtareas.add(tareaDesayuno) //costo = 13300
+      tareaDeMantenerDesayunos.nominaDeEmpleados().shouldBe(11)
+    }
   }
 })
